b10a2220b1c410a63379b0c17442461707094904
KRISTOPE <kitokgmy2@gmail.com>
2024-12-09 19:48:00 -0500

    tadi ada kendala

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "kipe.h"
+
+int main()
+{
+    printf("\t\t Library sistem operasi");
+    printf("\n ========================== \n");
+    printf("Welcome to converter money");
+    printf("\n ========================== \n");
+    printf("\n nilai dolar ke rupiah\n");
+    rupiah();
+    
+    printf("\n nilai rupiah ke dolar\n");
+    dolar();
+}
